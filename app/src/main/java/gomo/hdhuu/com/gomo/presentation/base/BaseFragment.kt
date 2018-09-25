3509b3d7cee85ad968074f5417a19f62e4bb179c
Hoang <hoangduchuuvn@gmail.com>
2018-09-25 17:40:21 +0700

    [DEV] add base Fragment view

--- app/src/main/java/gomo/hdhuu/com/gomo/presentation/base/BaseFragment.kt
+++ app/src/main/java/gomo/hdhuu/com/gomo/presentation/base/BaseFragment.kt
@@ -5,5 +5,30 @@ import android.support.v4.app.Fragment
 /**
  * Created by hoangduchuuvn@gmail.com on 9/22/18 .
  */
-open class BaseFragment: Fragment() {
+open class BaseFragment : Fragment(), BaseView {
+    override fun showloadingwithMsg(msg: String?, title: String?, type: Int, cancellable: Boolean) {
+        if (activity is BaseActivity)
+            (activity as BaseActivity).showloadingwithMsg(msg, title, type, cancellable)
+    }
+
+    override fun showloadingwithMsg(msg: String?, title: String?, type: Int) {
+        if (activity is BaseActivity)
+            (activity as BaseActivity).showloadingwithMsg(msg, title, type)
+    }
+
+    override fun showLoading(msg: String?, title: String?) {
+        if (activity is BaseActivity)
+            (activity as BaseActivity).showLoading(msg, title)
+    }
+
+    override fun showLoading() {
+        if (activity is BaseActivity)
+            (activity as BaseActivity).showLoading()
+    }
+
+    override fun hideLoading() {
+        if (activity is BaseActivity)
+            (activity as BaseActivity).hideLoading()
+    }
+
 }
---
+++ app/src/main/java/gomo/hdhuu/com/gomo/presentation/login/register/RegisterFragment.kt
@@ -0,0 +1,10 @@
+package gomo.hdhuu.com.gomo.presentation.login.register
+
+import gomo.hdhuu.com.gomo.presentation.base.BaseFragment
+
+/**
+ * Created by hoangduchuuvn@gmail.com on 9/25/18 .
+ */
+class RegisterFragment : BaseFragment(){
+
+}
