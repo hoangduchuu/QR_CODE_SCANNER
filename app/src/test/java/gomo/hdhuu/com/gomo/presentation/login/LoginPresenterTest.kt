34a44440320ced5deb778935d95d2478d4ca2be8
Hoang <hoangduchuuvn@gmail.com>
2018-10-01 22:17:46 +0700

    [DEV] update loginPresenterTest

--- app/src/test/java/gomo/hdhuu/com/gomo/presentation/login/LoginPresenterTest.kt
+++ app/src/test/java/gomo/hdhuu/com/gomo/presentation/login/LoginPresenterTest.kt
@@ -21,7 +21,9 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
+import javax.inject.Inject
 
 
 /**
@@ -29,74 +31,79 @@ import org.mockito.junit.MockitoJUnitRunner
  */
 @RunWith(MockitoJUnitRunner::class)
 class LoginPresenterTest {
-    //    var checkLogin = CheckLoginUsecaseWithFirebase()
-//
-//    lateinit var loginPresenter: LoginPresenter
-//
-    lateinit var firebaseAuth: FirebaseAuth
+    /**
+     * @view is View
+     */
+    @Inject
     @Mock
-    lateinit var view: LoginContract.View
+    lateinit var mockView: LoginContract.View
+    /**
+     *@mockFirebaseUser is mockFirebaseUser
+     */
     lateinit var mockFirebaseUser: FirebaseUser
-    //lateinit var mockLogin: LoginUsecaseWithFirebase
+
+    /**
+     *@presenter
+     */
+    @Mock
+    lateinit var presenter: LoginPresenter
+
+    /**
+     * @mockLogin is loginUsecase
+     */
+    @Mock
+    lateinit var mockLogin: LoginUsacase<AccountParams, AuthResult>
 
 
     @Before
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
         mockFirebaseUser = mock(FirebaseUser::class.java)
 
-        firebaseAuth = mock(FirebaseAuth::class.java)
+        presenter = LoginPresenter()
 
+        this.mockLogin = mock(LoginUsecaseWithFirebase::class.java)
+        this.mockView = mock(LoginContract.View::class.java)
+
+
+//        presenter.view = this.mockView
+        presenter.login = this.mockLogin
 
 
     }
 
 
-
     @Test
     fun whenLloginSuccess() {
 
-        val presenter: LoginPresenter = LoginPresenter()
+        val result = mock(AuthResult::class.java)
 
-        var mockLogin: LoginUsacase<AccountParams, AuthResult> = mock(LoginUsecaseWithFirebase::class.java)
-        var view: LoginContract.View = mock(LoginContract.View::class.java)
-        var result = mock(AuthResult::class.java)
-        presenter.login = mockLogin
-        presenter.view = view
-
-        `when`(mockLogin.buildUseCaseObservable(AccountParams("abc","abc")))
+        `when`(mockLogin.buildUseCaseObservable(AccountParams("abc", "abc")))
                 .thenReturn(Observable.just(result))
         //when
         presenter.doLogin("abc", "abc")
 
 
         //verify
-        verify(view).showLoading()
-        verify(view).hideLoading()
-        verify(view).gotoMainPage()
+        verify(mockView).showLoading()
+        verify(mockView).hideLoading()
+        verify(mockView).gotoMainPage()
     }
 
     @Test
     fun whenLloginFailed() {
+        val throwables = Throwable("abac")
 
-        val presenter: LoginPresenter = LoginPresenter()
-
-        var mockLogin: LoginUsacase<AccountParams, AuthResult> = mock(LoginUsecaseWithFirebase::class.java)
-        var view: LoginContract.View = mock(LoginContract.View::class.java)
-        presenter.login = mockLogin
-        presenter.view = view
-
-
-        var throwables = Throwable("abac")
-
-        `when`(mockLogin.buildUseCaseObservable(AccountParams("abc","abc")))
+        `when`(mockLogin.buildUseCaseObservable(AccountParams("abc", "abc")))
                 .thenReturn(Observable.error(throwables))
         //when
         presenter.doLogin("abc", "abc")
 
 
         //verify
-        verify(view).showLoading()
-        verify(view).onLoginErrors(throwables.localizedMessage)
+        verify(mockView).showLoading()
+        verify(mockView).onLoginErrors(throwables.localizedMessage)
 
     }
 
