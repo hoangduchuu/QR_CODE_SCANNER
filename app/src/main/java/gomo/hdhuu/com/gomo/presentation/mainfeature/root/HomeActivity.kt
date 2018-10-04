1f816748d499b8c7b28c8e456869af5449a626e7
Hoang <hoangduchuuvn@gmail.com>
2018-10-05 01:11:40 +0700

    [DEV] init nav icon click event and logout btn

--- app/src/main/java/gomo/hdhuu/com/gomo/presentation/mainfeature/root/HomeActivity.kt
+++ app/src/main/java/gomo/hdhuu/com/gomo/presentation/mainfeature/root/HomeActivity.kt
@@ -1,22 +1,35 @@
 package gomo.hdhuu.com.gomo.presentation.mainfeature.root
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.view.MenuItem
+import butterknife.OnClick
 import com.hnam.mvvm.replaceFragmentInActivity
 import gomo.hdhuu.com.gomo.GomoApp
 import gomo.hdhuu.com.gomo.R
 import gomo.hdhuu.com.gomo.presentation.base.BaseActivity
+import gomo.hdhuu.com.gomo.presentation.login.LoginActivity
 import gomo.hdhuu.com.gomo.presentation.mainfeature.OverView.OverViewFragment
+import kotlinx.android.synthetic.main.activity_home_app.*
+import javax.inject.Inject
 import kotlin.coroutines.experimental.EmptyCoroutineContext.plus
 
 /**
  * Created by hoangduchuuvn@gmail.com on 9/26/18 .
  */
-class HomeActivity : BaseActivity(), HomeContract.View {
+class HomeActivity : BaseActivity(), HomeContract.View, NavigationView.OnNavigationItemSelectedListener {
     /**
      * the Home component
      */
     var homeComponent: HomeContract.Component? = null
 
+    /**
+     * @presenter is presenter of activity
+     */
+
+    @Inject
+    lateinit var presenter: HomeContract.Presenter
 
     /**
      * setup view and bindind
@@ -37,10 +50,32 @@ class HomeActivity : BaseActivity(), HomeContract.View {
         homeComponent?.inject(this)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
     /**
      * the frame layout to show fragment in this activity
      */
     private fun getFragmentContaier(): Int {
         return R.id.fragment_container
+    }
+
+    override fun openLoginScreen() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        this.finish()
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.nav_logout -> {
+                presenter.logout()
+            }
+            else -> {
+                //nothing to do
+            }
+        }
+        return true
     }
 }
--- app/src/main/java/gomo/hdhuu/com/gomo/presentation/mainfeature/root/HomePresenter.kt
+++ app/src/main/java/gomo/hdhuu/com/gomo/presentation/mainfeature/root/HomePresenter.kt
@@ -1,5 +1,8 @@
 package gomo.hdhuu.com.gomo.presentation.mainfeature.root
 
+import com.google.firebase.auth.FirebaseAuth
+import durdinapps.rxfirebase2.RxFirebaseAuth
+import durdinapps.rxfirebase2.RxFirebaseUser
 import javax.inject.Inject
 
 /**
@@ -8,6 +11,18 @@ import javax.inject.Inject
 class HomePresenter
 @Inject
 constructor() : HomeContract.Presenter {
+    /**
+     * @View is the view of activity
+     */
+    @Inject
+    lateinit var view: HomeContract.View
+
+    // create signout usecae late
+    //todo
+    override fun logout() {
+        FirebaseAuth.getInstance().signOut()
+        view.openLoginScreen()
+    }
 
 
 }
